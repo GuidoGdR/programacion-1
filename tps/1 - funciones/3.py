b72710e7b59a5d8fd1f898107cf79ab727e97698
GuidoGdR <guidodorego@gmail.com>
2025-08-07 12:49:46 -0300

    Agrego los enunciados de los ejercicios como comentario

---
+++ tps/1 - funciones/3.py
@@ -0,0 +1,9 @@
+"""
+Una persona desea llevar el control de los gastos realizados al viajar en el subterráneo dentro de un mes.
+Sabiendo que dicho medio de transporte utiliza un esquema de tarifas decrecientes 
+(detalladas en la tabla de abajo) se solicita desarrollar una función que reciba como parámetro 
+la cantidad de viajes realizados en un determinado mes y devuelva el total gastado en viajes. 
+
+Realizar también un programa para verificar el comportamiento de la función.
+"""
+#tabla en el pdf punto 3 
