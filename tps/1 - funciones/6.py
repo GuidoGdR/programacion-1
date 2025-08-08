537b06ab345e016190bccefb8db1e8084c9a9d6c
GuidoGdR <guidodorego@gmail.com>
2025-08-08 15:13:15 -0300

    Soluciono el problema 6

--- tps/1 - funciones/6.py
+++ tps/1 - funciones/6.py
@@ -1,3 +1,26 @@
 """
-Desarrollar una función que reciba como parámetros dos números enteros positivos y devuelva como valor de retorno el número que resulte de concatenar ambos parámetros. Por ejemplo, si recibe 1234 y 567 debe devolver 1234567. No se permite utilizar facilidades de Python no vistas en clase.
+Desarrollar una función que reciba como parámetros dos números enteros positivos y devuelva 
+como valor de retorno el número que resulte de concatenar ambos parámetros. Por ejemplo, 
+si recibe 1234 y 567 debe devolver 1234567. 
+No se permite utilizar facilidades de Python no vistas en clase.
 """
+def count_digits_of_number(number:int)->int:
+    if number < 0: number = number * -1
+
+    if number <= 10: return 1
+
+    count = 1
+
+    while True:
+        calc_result = 10*count
+        
+        if calc_result == number: return count
+
+        if calc_result > number: return count - 1
+
+        count += 1
+
+def union_strings_sin_ser_strings_y_retornando_int(a:int, b:int)->int:
+    return a * ( 10**count_digits_of_number(b) ) + b
+
+print(union_strings_sin_ser_strings_y_retornando_int(1,2))
