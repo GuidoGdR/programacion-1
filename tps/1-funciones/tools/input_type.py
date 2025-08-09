2f71c7c77937ed42f9888bb9585342d3f12effb9
Guido do Rego <guidodorego@gmail.com>
2025-08-09 16:18:42 -0300

    Creo la carpeta tools y genero input_type como su primer herramienta (Hecha para facilitar interacciones con el usuario y garantizar un input valido)

---
+++ tps/1-funciones/tools/input_type.py
@@ -0,0 +1,31 @@
+from typing import Type, Callable, Any, Union
+
+def input_type(input_message="Ingrese un valor: ", input_type:Type=str, error_message="Valor incorrecto", validator:Callable[[Any], tuple[Union[str, None], Any]]=lambda x:(None, x)):
+    while True:
+        usr_input = input(input_message)
+                
+        if input_type == int or input_type == float:
+            
+            if not usr_input.isnumeric():
+                print(error_message)
+                continue
+            
+            usr_input = input_type(usr_input)
+            (validation_error_message, usr_input) = validator(usr_input)
+            
+            if validation_error_message != None:
+                print(validation_error_message)
+                continue
+
+            return usr_input
+
+        if input_type == str:
+            
+            usr_input = input_type(usr_input)
+            (validation_error_message, usr_input) = validator(usr_input)
+
+            if validation_error_message != None:
+                print(validation_error_message)
+                continue
+
+            return usr_input
