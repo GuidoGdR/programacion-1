40901f393e973f6af712127c329e8f4789794b79
Guido do Rego <guidodorego@gmail.com>
2025-08-09 12:42:08 -0300

    Resuelvo el ejercicio 4

--- tps/1-funciones/4.py
+++ tps/1-funciones/4.py
@@ -11,3 +11,59 @@ debido a falta de billetes con denominaciones adecuadas.
 Ejemplo: Si la compra es de $3170 y se abona con $5000, el vuelto debe contener 1 billete de $1000, 
 1 billete de $500, 1 billete de $200, 1 billete de $100 y 3 billetes de $10.
 """
+# billetes de $5000, $1000, $500, $200, $100, $50 y $10. 
+from typing import Union
+
+def calculate_change(total:float, pay:float)->tuple[Union[None, Exception], tuple[int], tuple[int]]:
+    money_types = (5000, 1000, 500, 200, 100, 50, 10)
+    money_types_len = money_types.__len__()
+    
+    if total > pay:
+        return (ValueError("Error, el direno recibido es insuficiente."), money_types, (0) * money_types_len)
+    
+    if  pay % 10 != 0:
+        return (ValueError("Error, no hay billetes de denominacion adecuada."), money_types, (0) * money_types_len)
+    print(pay % 10 )
+
+    results:list[int, int, int, int, int, int, int] = [0] * money_types_len
+
+    pay = pay - total
+    for money_type_index in range(money_types_len):
+        money_type = money_types[money_type_index]
+
+        if pay >= money_type:
+
+            n_of_this = pay / money_type
+            
+            spare = n_of_this - int(n_of_this)
+
+            results[money_type_index] = int(n_of_this)
+            
+            pay = int(spare * money_type)
+
+            if pay == 0:
+                break
+    
+    return ( None, money_types, (results[:]) )
+
+def interface():
+    while True:
+            
+        total = float(input("Importe a cobrar: "))
+        pay = float(input("Monto pagado: "))
+        
+        [err, money_types, quantities] = calculate_change(total, pay)
+
+        if err:
+            print(f"{err.__str__()}")
+
+        else:
+            for i in range(money_types.__len__()):
+
+                print(f"{quantities[i]}\tde\t{money_types[i]}")
+
+        if input("-1 para salir, cualquier otra cosa para continuar") == "-1":
+            break
+
+
+interface()
